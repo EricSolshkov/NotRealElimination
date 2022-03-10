bf6eb3b524d77dabb98bf19e5313076464681989
EricPu <779781675@qq.com>
2022-03-10 22:55:23 +0800

    推进中： TileAsset、TextAsset的自定义UI

--- Assets/Editor/AssetManagerEditor.cs
+++ Assets/Editor/AssetManagerEditor.cs
@@ -7,67 +7,21 @@ namespace MatchEngine
     [CustomEditor(typeof(AssetManager))]
     public class AssetManagerEditor : Editor
     {
-        private ReorderableList _tileTypeList;
-
+        
+        
         private void OnEnable()
         {
-            //创建ReorderableList 参数：
-            //arg1:序列化物体，arg2:序列化数据，arg3:可否拖动，arg4:是否显示标题，arg5:是否显示添加按钮，arg6:是否显示添加按钮
-            _tileTypeList = new ReorderableList(serializedObject, serializedObject.FindProperty("tileTypeContainer")
-                , true, true, true, true);
-
-            //自定义列表名称
-            _tileTypeList.drawHeaderCallback = (Rect rect) =>
-            {
-                GUI.Label(rect, "Tile Types");
-            };
-
-            //定义元素的高度
-            _tileTypeList.elementHeight = 80;
-
-            //自定义绘制列表元素
-            /*_tileTypeList.drawElementCallback = (Rect rect, int index, bool selected, bool focused) =>
-            {
-                //根据index获取对应元素 
-                SerializedProperty item = _tileTypeList.serializedProperty.GetArrayElementAtIndex(index);
-                var assetItem = new SerializedObject(item.objectReferenceValue);
-                rect.height -= 4;
-                rect.y += 2;
-
-                /*EditorGUI.PropertyField(
-                    new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
-                    assetItem.FindProperty("name"),
-                    new GUIContent("Element " + index));
-                EditorGUI.TextField(
-                    new Rect(rect.x+64, rect.y, 128, EditorGUIUtility.singleLineHeight),
-                    "name",
-                    assetItem.FindProperty("name").stringValue
-                    );
-                EditorGUI.ObjectField(
-                    new Rect(rect.x + 256, rect.y, 64, 64),
-                    assetItem.FindProperty("sprite"),
-                    typeof(Texture), GUIContent.none) ;
-            };*/
-
-            //当删除元素时候的回调函数，实现删除元素时，有提示框跳出
-            _tileTypeList.onRemoveCallback = (ReorderableList list) =>
-            {
-                if (EditorUtility.DisplayDialog("Warnning", "Do you want to remove this element?", "Remove", "Cancel"))
-                {
-                    ReorderableList.defaultBehaviours.DoRemoveButton(list);
-                }
-            };
+            
         }
 
 
         public override void OnInspectorGUI()
         {
             //更新
-            serializedObject.Update();
+            //serializedObject.Update();
             //自动布局绘制列表
-            _tileTypeList.DoLayoutList();
             //应用
-            serializedObject.ApplyModifiedProperties();
+            //serializedObject.ApplyModifiedProperties();
             
             DrawDefaultInspector();
             AssetManager AM = (AssetManager)target;
--- Assets/Editor/TileTypeDrawer.cs
+++ Assets/Editor/TileTypeDrawer.cs
@@ -8,44 +8,39 @@ namespace MatchEngine
     [CustomPropertyDrawer(typeof(TileTypeAsset))]
     public class TileTypeDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 90;
+        }
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             using (new EditorGUI.PropertyScope(position, label, property))
             {
-
-
-                //设置属性名宽度
-                EditorGUIUtility.labelWidth = 60;
-                position.height = EditorGUIUtility.singleLineHeight;
+                var nameRect = new Rect(position)
+                {
+                    x = position.x + 80,
+                    width = position.width - 80,
+                    height = EditorGUIUtility.singleLineHeight
+                };
                 
-                position.height = 80;
-
-                EditorGUI.Foldout(position, false, ".?") ;
-                //
                 Rect iconRect = new Rect(position)
                 {
+                    //x = position.x + position.width-64,
+                    y = position.y,// + EditorGUIUtility.singleLineHeight+5,
                     width = 64,
                     height = 64
                 };
 
-                var nameRect = new Rect(position)
-                {
-                    width = position.width - 80,
-                    x = position.x + 80
-                };
-
-
-
-                //
                 var _property = new SerializedObject(property.objectReferenceValue);
                 SerializedProperty iconProperty = _property.FindProperty("sprite");
                 SerializedProperty nameProperty = _property.FindProperty("name");
 
-
                 //EditorGUI.TextField(nameRect, property.objectReferenceValue.name);
                 EditorGUI.TextField(nameRect, "name", nameProperty.stringValue);
-                EditorGUI.ObjectField(iconRect, iconProperty, typeof(Texture));
+                var spriteBox = EditorGUI.ObjectField(iconRect,iconProperty.objectReferenceValue, typeof(Sprite), false);
+                //EditorGUI.ObjectField(iconRect,iconProperty.objectReferenceValue, typeof(Sprite), false);
                 //EditorGUI.ObjectField(iconRect, property.objectReferenceValue.sprite, typeof(Texture), false);
+
                 
                 
 
